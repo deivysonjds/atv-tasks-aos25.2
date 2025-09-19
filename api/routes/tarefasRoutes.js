c4d7ccc30c94b7f31eea1836a2c5853e35062f03
Deivyson J. Silva <139878479+deivysonjds@users.noreply.github.com>
2025-09-19 02:31:21 +0000

    added: routes

--- api/routes/tarefasRoutes.js
+++ api/routes/tarefasRoutes.js
@@ -12,26 +12,45 @@ router.get("/", (req, res)=>{
     return res.status(200).json(listarTarefas)
 })
 
-router.get("/:id", (req, res)=>{
-    const id = req.params.id
-
-    res.json(`task ${id}`)
+router.get("/:objectId", (req, res)=>{
+    const objectId = req.params.objectId
+    let tarefa = mostrarTarefaPeloId(objectId)
+    if (tarefa === null) return res.status(404).send("Tarefa não localizado");
+    return res.status(200).json(tarefa)
 })
 
 router.post("/", (req, res)=>{
-    const data = req.body
-    res.send(`new task ${data}`)
+    const {descricao, concluida} = req.body
+    if (descricao===undefined) return res.status(400).send("Descrição não informada")
+    
+    try {
+        let tarefa = criarTarefa(descricao, concluida);
+        return res.status(201).json(tarefa);
+    } catch (error) {
+        return res.status(400).send(error)
+    }
 })
 
-router.delete("/:id", (req, res)=>{
-    const id = req.params.id
-    res.send(`delete task ${id}`)
+router.delete("/:objectId", (req, res)=>{
+    const objectId = req.params.objectId
+
+    let deletado = deletarTarefaPorId(objectId)
+    if (!deletado){
+        return res.status(404).send("Tarefa não encontrada")
+    }
+    res.status(200)
 })
 
-router.put("/:id", (req, res)=>{
-    const id = req.params.id
+router.put("/:objectId", (req, res)=>{
+    const objectId = req.params.objectId
     const data = req.body
-    res.send(`put task ${id} - body ${data}`)
+
+    try {
+        let tarefa = modificarTarefa(objectId, data);
+        return res.status(200).send(tarefa)
+    } catch (error) {
+        return res.status(404).send("Tarefa não localizada")
+    }
 })
 
 export default router;
